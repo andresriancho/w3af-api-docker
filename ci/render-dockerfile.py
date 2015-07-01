d28ca4097be27a187a57352242aa5edc9ec8977d
Andres Riancho <andres.riancho@gmail.com>
2015-07-01 15:56:16 -0300

    Improved rendering

--- ci/render-dockerfile.py
+++ ci/render-dockerfile.py
@@ -4,7 +4,6 @@ from __future__ import print_function
 
 import os
 import sys
-import requests
 
 from jinja2 import Template
 
@@ -25,12 +24,17 @@ def main():
     if latest_w3af_commit is None:
         print('No W3AF_COMMIT environment variable was specified! When we'
               ' build in this scenario there is a (big) chance of us'
-              ' trying to use a Dockerfile FROM which will NOT EXIST!')
-        latest_w3af_commit = get_latest_commit(current_branch)
+              ' trying to use a Dockerfile FROM which will NOT EXIST!'
+              '\n\n'
+              'Consider using the ci/build-docker.py script to manually build'
+              ' this repository or code the get_latest_w3af_image_tag function'
+              ' to retrieve the latest tag from the docker registry.')
+        return 1
 
     render(latest_w3af_commit, current_branch)
 
     print('Using w3af commit: %s' % latest_w3af_commit)
+    return 0
 
 
 def render(w3af_commit, current_branch):
@@ -47,15 +51,15 @@ def render(w3af_commit, current_branch):
     file(OUTPUT_FILE, 'w').write(rendered_dockerfile)
 
 
-def get_latest_commit(current_branch):
+def get_latest_w3af_image_tag(current_branch):
     """
-    Uses the github API to retrieve the latest commit from the w3af repository
+    Uses the registry API to retrieve the latest image tag for the corresponding
+    branch.
 
-    :return: The commit it
+    :return: The branch name, something like '<commit-id>-develop' or
+             '<commit-id>-master'
     """
-    #GET /repos/:owner/:repo/commits
-    url = 'https://api.github.com/repos/andresriancho/w3af/commits?sha=%s'
-    return requests.get(url % current_branch).json()[0]['sha'][:7]
+    raise NotImplementedError
 
 
 if __name__ == '__main__':
