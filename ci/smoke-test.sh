bb50651f53ab117c2ad1645280fb2efa72a4f14c
Andres Riancho <andres.riancho@gmail.com>
2015-07-02 15:35:59 -0300

    sleep, wait for the container to start

--- ci/smoke-test.sh
+++ ci/smoke-test.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+# Start the docker container that we just built
 docker run -d \
            -v ~/.w3af:/root/.w3af \
            -v ~/w3af-shared:/root/w3af-shared \
            -p 5000:5000 \
            -p 9001:9001 \
-           andresriancho/w3af-api
+           andresriancho/w3af-api:latest
 
+# Wait for the container to start
+sleep 20
+
+# Run the tests
 git clone https://github.com/andresriancho/w3af-api-client.git
 cd w3af-api-client
 nosetests -v -s ci
